f33c4a360db16fad5b0b97c7ee8f18a34c89f3a7
Spren11 <196334223+Spren11@users.noreply.github.com>
2025-08-22 00:56:51 +0700

    Add Hp and Dead sound

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -3,10 +3,27 @@
 extends Node2D
 
 var score : int = 0
+var hp :int = 100
 
 # Adds 1 to score variable
 func add_score():
 	score += 1
+
+# Call this function from anywhere when the player needs to respawn
+func respawn_player():
+	# Reset the HP to its maximum value
+	hp = 100
+
+	# Reload the current scene to restart the level
+	get_tree().reload_current_scene()
+
+	# If you don't want to reload the whole scene, you would instead
+	# reset the player's position and other states here.
+
+func damage(dm):
+	hp -= dm
+	#player.death_particles.emitting = true
+	AudioManager.get_node("DeathSfx").play()
 
 # Loads next level
 func load_next_level(next_scene : PackedScene):
--- Scripts/GameUI.gd
+++ Scripts/GameUI.gd
@@ -2,7 +2,9 @@ extends Control
 
 @onready var score_texture = %Score/ScoreTexture
 @onready var score_label = %Score/ScoreLabel
+@onready var hp_label: Label = %Score/HPLabel
 
 func _process(_delta):
 	# Set the score label text to the score variable in game maanger script
 	score_label.text = "x %d" % GameManager.score
+	hp_label.text = "HP %d" % GameManager.hp
