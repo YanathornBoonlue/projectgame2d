4333cf7a3b093f7c876e7e4877d8ab69b59b1488
thipataim-art <thipatai.m@kkumail.com>
2025-08-26 19:32:10 +0700

    Update GameUI.gd

--- Scripts/GameUI.gd
+++ Scripts/GameUI.gd
@@ -4,7 +4,7 @@ extends Control
 @onready var score_label = %Score/ScoreLabel
 @onready var hp_label: Label = %Score/HPLabel
 
-func _process(_delta):
-	# Set the score label text to the score variable in game maanger script
-	score_label.text = "x %d" % GameManager.score
-	hp_label.text = "HP %d" % GameManager.hp
+#func _process(_delta):
+	## Set the score label text to the score variable in game maanger script
+	#score_label.text = "x %d" % GameManager.score
+	#hp_label.text = "HP %d" % GameManager.hp
